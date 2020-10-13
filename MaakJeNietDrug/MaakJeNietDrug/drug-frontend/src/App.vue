2cb2cedf7cad55d9c585694a17bb6b5d3722e4ca
Kim Siero <kimsiero@gmail.com>
2020-10-13 11:42:50 +0200

    Routing and button
    Fixed weird error with toggle

--- MaakJeNietDrug/MaakJeNietDrug/drug-frontend/src/App.vue
+++ MaakJeNietDrug/MaakJeNietDrug/drug-frontend/src/App.vue
@@ -1,93 +1,40 @@
-
-<template>
+  <template>
   <div id="app">
-    
-    <AddNewMedicine v-on:add-medicine="AddNewMed"  />
-    <div class="container">
-      <div class="card mt-5">
-        <MedicineList
-          v-bind:medicineList="medicineList"
-          v-on:inspect-medicine="InspectMedicine"
-          v-on:del-medicine="DeleteMed"
-        />
-      </div>
-    </div>
+    <Header />
+    <router-view/>
   </div>
 </template>
 
-
-
 <script>
-import MedicineList from "./components/MedicineList";
-import AddNewMedicine from "./components/AddNewMedicine";
-import axios from "axios";
-import Vue from "vue";
-import VueResizeText from "vue-resize-text";
-Vue.use(VueResizeText);
-
+import Header from './components/layout/Header';
 export default {
-  name: "App",
+  name:"app",
   components: {
-    MedicineList,
-    AddNewMedicine,
-  },
-  data() {
-    return {
-      medicineList: [],
-    };
-  },
-  methods: {
-    AddNewMed(newMed) {
-      axios
-        .post(
-          "https://i338995core.venus.fhict.nl/Medicine/Add/" +
-            newMed.name +
-            "/" +
-            newMed.description
-        )
-        .then((res) => (this.medicineList = [...this.medicineList, res.data]))
-        .catch((err) => console.log(err));
-    },
-    DeleteMed(obj) {
-      axios
-        .delete("https://i338995core.venus.fhict.nl/Medicine/Delete/" + obj.id)
-
-        .then(
-          // eslint-disable-next-line
-          (res) =>
-            (this.medicineList = this.medicineList.filter(
-              (medicine) => medicine.id !== obj.id
-            ))
-        )
-        .catch((err) => console.log(err));
-    },
-    InspectMedicine(obj) {
-      console.log("inspect button clicked on: " + obj.id);
-    },
-  },
-  // THIS CODE RUNS WHEN A NEW VUE INSTANCE IS CREATED (AKA WHEN THE TABLE IS CALLED FIRST)
-  created() {
-    axios
-      .get("https://i338995core.venus.fhict.nl/Medicine/GetAll")
-      .then((res) => (this.medicineList = res.data))
-      .catch((err) => console.log(err));
-  },
-  updated() {
-    axios
-      .get("https://i338995core.venus.fhict.nl/Medicine/GetAll")
-      .then((res) => (this.medicineList = res.data))
-      .catch((err) => console.log(err));
-  },
-};
+    Header
+  }
+}
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+  .btn:hover {
+    background: #666;
+  }
 </style>
